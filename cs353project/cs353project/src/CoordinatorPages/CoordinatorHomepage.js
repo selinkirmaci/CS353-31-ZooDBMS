fd7caa5cc1a367795bbfc88deb2877b886ee9899
selinkirmaci <selinkirmaci08@gmail.com>
2021-04-08 01:31:10 +0300

    CoordinatorHomepage started--added Tabs updated

---
+++ cs353project/cs353project/src/CoordinatorPages/CoordinatorHomepage.js
@@ -0,0 +1,145 @@
+import React, {Component} from 'react';
+import Tabs from "@material-ui/core/Tabs";
+import Tab from "@material-ui/core/Tab";
+import makeStyles from "@material-ui/core/styles/makeStyles";
+import Box from "@material-ui/core/Box";
+import Typography from "@material-ui/core/Typography";
+import PropTypes from "prop-types";
+import {useTheme} from "@material-ui/core/styles";
+import SwipeableViews from "react-swipeable-views";
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+
+function TabPanel(props) {
+    const { children, value, index, ...other } = props;
+
+    return (
+        <div
+            role="tabpanel"
+            hidden={value !== index}
+            id={`full-width-tabpanel-${index}`}
+            aria-labelledby={`full-width-tab-${index}`}
+            {...other}
+        >
+            {value === index && (
+                <Box p={3}>
+                    <Typography>{children}</Typography>
+                </Box>
+            )}
+        </div>
+    );
+}
+TabPanel.propTypes = {
+    children: PropTypes.node,
+    index: PropTypes.any.isRequired,
+    value: PropTypes.any.isRequired,
+};
+function a11yProps(index) {
+    return {
+        id: `full-width-tab-${index}`,
+        'aria-controls': `full-width-tabpanel-${index}`,
+    };
+}
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        flexGrow: 1,
+        backgroundColor: theme.palette.background.paper,
+    },
+    container: {
+        maxHeight: 440,
+    },
+    table: {
+        minWidth: 650,
+    },
+}));
+function createData(name, calories, fat, carbs, protein) {
+    return { name, calories, fat, carbs, protein };
+}
+const rows = [
+    createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
+    createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
+    createData('Eclair', 262, 16.0, 24, 6.0),
+    createData('Cupcake', 305, 3.7, 67, 4.3),
+    createData('Gingerbread', 356, 16.0, 49, 3.9),
+];
+function CoordinatorHomepage() {
+    const classes = useStyles();
+    const [value, setValue] = React.useState(0);
+    const theme = useTheme();
+    const handleChange = (event, newValue) => {
+        setValue(newValue);
+    };
+    const handleChangeIndex = (index) => {
+        setValue(index);
+    };
+    return(
+        <div>
+                <div >
+                    <Tabs
+                        value={value}
+                        onChange={handleChange}
+                        indicatorColor="primary"
+                        textColor="primary"
+                        centered
+                    >
+                        <Tab label="Group Tours">
+                        </Tab>
+                        <Tab label="Educational Programs" />
+                        <Tab label="Conservational Organizations" />
+                    </Tabs>
+
+                    <SwipeableViews
+                        axis={theme.direction === 'rtl' ? 'x-reverse' : 'x'}
+                        index={value}
+                        onChangeIndex={handleChangeIndex}
+                    >
+                        <TabPanel value={value} index={0} dir={theme.direction}>
+                            <h1>Group Tours</h1>
+                            <TableContainer component={Paper}>
+                                <Table className={classes.table} aria-label="simple table">
+                                    <TableHead>
+                                        <TableRow>
+                                            <TableCell>Dessert (100g serving)</TableCell>
+                                            <TableCell align="right">Calories</TableCell>
+                                            <TableCell align="right">Fat&nbsp;(g)</TableCell>
+                                            <TableCell align="right">Carbs&nbsp;(g)</TableCell>
+                                            <TableCell align="right">Protein&nbsp;(g)</TableCell>
+                                        </TableRow>
+                                    </TableHead>
+                                    <TableBody>
+                                        {rows.map((row) => (
+                                            <TableRow key={row.name}>
+                                                <TableCell component="th" scope="row">
+                                                    {row.name}
+                                                </TableCell>
+                                                <TableCell align="right">{row.calories}</TableCell>
+                                                <TableCell align="right">{row.fat}</TableCell>
+                                                <TableCell align="right">{row.carbs}</TableCell>
+                                                <TableCell align="right">{row.protein}</TableCell>
+                                            </TableRow>
+                                        ))}
+                                    </TableBody>
+                                </Table>
+                            </TableContainer>
+                        </TabPanel>
+                        <TabPanel value={value} index={1} dir={theme.direction}>
+                            <h1>Educational Programs</h1>
+                        </TabPanel>
+                        <TabPanel value={value} index={2} dir={theme.direction}>
+                            <h1>Conservational Organizations</h1>
+                        </TabPanel>
+
+                    </SwipeableViews>
+                </div>
+
+
+        </div>
+    );
+}
+export default CoordinatorHomepage;
